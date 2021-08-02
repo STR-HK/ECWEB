3c0628599f574b1b505541e0d8fe60353db3187a
STR-HK <78652371+STR-HK@users.noreply.github.com>
2021-08-02 14:46:51 +0900

    update

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import uvicorn
 from starlette.requests import Request
 from datetime import datetime
 import sqlite3
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -11,20 +12,20 @@ cursor = connect.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS log(time TEXT, ip TEXT)")
 
 
-@app.get("/pull.js")
-def root(request: Request):
-    ip = request.client.host
-    time = datetime.utcnow()
+class Post(BaseModel):
+    ip: str
+    time: str
+
+
+@app.get("/get")
+def get(ip: str, time: str):
     print("[{}] [WEB/PULL] {}".format(time, ip))
-    cursor.execute("INSERT INTO log(time, ip) VALUES (?,?)", (time, ip))
+    cursor.execute(
+        "INSERT INTO log(time, ip) VALUES (?,?)",
+        (time, ip),
+    )
     connect.commit()
 
 
 if __name__ == "__main__":
-    uvicorn.run(
-        "server:app",
-        host="192.168.0.2",
-        port=8123,
-        ssl_keyfile="./key.pem",
-        ssl_certfile="./cert.pem",
-    )
+    uvicorn.run("server:app", host="192.168.0.2", port=8123)
