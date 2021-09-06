4ca9980878102d45c435909a637083d45a6e783f
STR-HK <78652371+STR-HK@users.noreply.github.com>
2021-09-06 22:45:12 +0900

    detail update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,59 @@
-var now = Date.now();
-var exam = Date.parse('2021-10-12 09:00:00')
-var calced = exam - now
-var left = Math.floor(calced / (24 * 60 * 60 * 1000))
-var viewer = document.getElementsByClassName('card__preci')[0]
-viewer.innerHTML = `${left} Days Left`
+let dateleftviewer = document.getElementById('dateleft')
+let leftviwer = document.getElementById('left')
+let changebtn = document.getElementById('change')
+
+let fd, fH, fM, fS = 0
+let d, H, M, S = 0
+
+let exam = Date.parse('2021-10-12 09:00:00')
+
+function zeroFill( number, width )
+{
+  width -= number.toString().length;
+  if ( width > 0 )
+  {
+    return new Array( width + (/\./.test( number ) ? 2 : 1) ).join( '0' ) + number;
+  }
+  return number + ""; // always return a string
+}
+
+function calc() {
+    now = Date.now()
+    var calced = exam - now
+    dateleft = Math.floor(calced / (24 * 60 * 60 * 1000))
+
+    dateleftviewer.innerHTML = `${dateleft} Days Left`
+    
+    fS = calced / (1000)
+    fM = calced / (1000 * 60)
+    fH = calced / (1000 * 60 * 60)
+    fd = calced / (1000 * 60 * 60 * 24)
+
+    d = Math.floor(fd)
+    H = Math.floor(fH) - ( d * 24 )
+    M = Math.floor(fM) - ( d * 24 * 60 ) - ( H * 60 )
+    S = Math.floor(fS) - ( d * 24 * 60 * 60 ) - ( H * 60 * 60 ) - ( M * 60 )
+
+    H = zeroFill(H, 2)
+    M = zeroFill(M, 2)
+    S = zeroFill(S, 2)
+
+    left = `${d} : ${H} : ${M} : ${S}`
+    leftviwer.innerText = left
+}
+
+calc()
+
+function change() {
+    if (dateleftviewer.style.display != 'none') {
+        dateleftviewer.style.display = 'none'
+        leftviwer.style.display = 'block'
+        changebtn.style.boxShadow = 'inset 0 0 4px 0 rgb(0 0 0)'
+    } else {
+        dateleftviewer.style.display = 'block'
+        leftviwer.style.display = 'none'
+        changebtn.style.boxShadow = '0 0 4px 0 rgb(0 0 0)'
+    }
+}
+
+setInterval(calc, 1000)
